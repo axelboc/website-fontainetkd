06258a96090fe2fba7ea11dd9e247dbaf62ae7db
Axel Bocciarelli <axelboc@gmail.com>
2020-03-21 18:14:11 +0100

    Style Horaires section on desktop

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -4,11 +4,11 @@ import React from 'react';
 import styles from './Heading.module.css';
 
 function Heading(props) {
-  const { minor, sub, level = 2, children } = props;
+  const { sub, level = 2, children } = props;
 
   const Tag = `h${level}`;
   return (
-    <Tag className={styles.heading} data-major={!minor || undefined}>
+    <Tag className={styles.heading}>
       <span className={styles.title}>{children}</span>
       {sub && <span className={styles.sub}>( {sub} )</span>}
     </Tag>
@@ -16,7 +16,6 @@ function Heading(props) {
 }
 
 Heading.propTypes = {
-  minor: PropTypes.bool,
   sub: PropTypes.string,
   level: PropTypes.number,
   children: PropTypes.node,
--- src/components/Horaires.js
+++ src/components/Horaires.js
@@ -9,38 +9,45 @@ const ADDRESSES = [
   'Parc Karl Marx, 38170 Seyssinet-Pariset, France',
 ];
 
-const MAP_DIMENSIONS = [640, 640];
+const MAP_DIMENSIONS = [500, 500];
 const MAP_URL = buildMapUrl(ADDRESSES, ...MAP_DIMENSIONS);
 
 function Horaires() {
   return (
-    <section id="horaires">
-      <Heading>Horaires</Heading>
-      <div className={styles.content}>
-        <div className={styles.times}>
-          <h3>Mardi - Gymnase Robert Vial</h3>
-          <ul>
-            <li>18h - 19h : Section enfants</li>
-            <li>19h15 - 21h : Section ados &amp; adultes</li>
-          </ul>
-          <h3>Mercredi - Parc Karl Marx</h3>
-          <ul>
-            <li>19h30 - 21h : Section ados &amps; adults</li>
-            <li>21h - 22h : Entraînement libre</li>
-          </ul>
-          <h3>Vendredi - Parc Karl Marx</h3>
-          <ul>
-            <li>20h30 - 22h : Section ados &amps; adults</li>
-          </ul>
-        </div>
-        <div className={styles.map}>
-          <div className={styles.tape} />
-          <img
-            className={styles.mapImg}
-            srcSet={`${MAP_URL} 1x, ${MAP_URL}&scale=2 2x`}
-            width={MAP_DIMENSIONS[0]}
-            height={MAP_DIMENSIONS[1]}
-          />
+    <section id="horaires" className={styles.root}>
+      <div className={styles.inner}>
+        <Heading>Horaires</Heading>
+        <div className={styles.content}>
+          <div className={styles.map}>
+            <img
+              className={styles.mapImg}
+              srcSet={`${MAP_URL} 1x, ${MAP_URL}&scale=2 2x`}
+              width={MAP_DIMENSIONS[0]}
+              height={MAP_DIMENSIONS[1]}
+            />
+          </div>
+          <div className={styles.times}>
+            <article className={styles.entry}>
+              <h3>Mardi - Gymnase Robert Vial</h3>
+              <ul>
+                <li>18h - 19h : Section enfants</li>
+                <li>19h15 - 21h : Section ados &amp; adultes</li>
+              </ul>
+            </article>
+            <article className={styles.entry}>
+              <h3>Mercredi - Parc Karl Marx</h3>
+              <ul>
+                <li>19h30 - 21h : Section ados &amps; adults</li>
+                <li>21h - 22h : Entraînement libre</li>
+              </ul>
+            </article>
+            <article className={styles.entry}>
+              <h3>Vendredi - Parc Karl Marx</h3>
+              <ul>
+                <li>20h30 - 22h : Section ados &amps; adults</li>
+              </ul>
+            </article>
+          </div>
         </div>
       </div>
     </section>
