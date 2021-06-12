4fa98b13dcc2bbdfbfa48146f616fd9cd8395b8a
Axel Bocciarelli <axelboc@gmail.com>
2021-06-12 18:13:25 +0200

    Update times

--- src/components/Horaires.js
+++ src/components/Horaires.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { FiExternalLink } from 'react-icons/fi';
+import { FiExternalLink, FiSun } from 'react-icons/fi';
 
 import { Times } from '../data';
 import Heading from './Heading';
@@ -11,64 +11,75 @@ function Horaires() {
       <div id="horaires" className={styles.inner}>
         <Heading>Horaires</Heading>
         <div className={styles.content}>
-          {Object.entries(Times).map(([day, times], colIndex) => (
-            <Fragment key={day}>
-              <h3
-                className={styles.day}
-                style={{ gridColumn: colIndex + 1, gridRow: 1 }}
-              >
-                {day}
-              </h3>
-              {colIndex > 0 && (
-                <span
-                  className={styles.dayBorder}
-                  style={{ gridColumn: colIndex + 1, gridRow: '1 / span 16' }}
-                />
-              )}
-              {times.map(({ from, to, gridRow, section, location }) => (
-                <div
-                  key={`${from}-${to}`}
-                  className={styles.entry}
-                  style={{ gridColumn: colIndex + 1, gridRow }}
+          <div className={styles.contentInner}>
+            {Object.entries(Times).map(([day, times], colIndex) => (
+              <Fragment key={day}>
+                <h3
+                  className={styles.day}
+                  style={{ gridColumn: colIndex + 1, gridRow: 1 }}
                 >
-                  <span className={styles.time}>
-                    {from}
-                    <span>–</span>
-                    {to}
-                  </span>
-                  <span className={styles.section}>{section}</span>
-                  <a
-                    className={styles.locationLink}
-                    href={location.gmapUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {location.name}
-                    <FiExternalLink className={styles.externalIcon} />
-                  </a>
-                </div>
-              ))}
-            </Fragment>
-          ))}
-          <span className={styles.midday} />
+                  {day}
+                </h3>
+                {colIndex > 0 && (
+                  <span
+                    className={styles.dayBorder}
+                    style={{ gridColumn: colIndex + 1 }}
+                  />
+                )}
+                {times.map(
+                  ({
+                    from,
+                    to,
+                    duration,
+                    start,
+                    section,
+                    location,
+                    variant,
+                  }) => (
+                    <div
+                      key={`${from}-${to}`}
+                      className={styles.entry}
+                      style={{
+                        gridColumn: colIndex + 1,
+                        gridRow: `${start + 1} / span ${duration * 4}`,
+                      }}
+                      data-variant={variant}
+                    >
+                      <span className={styles.time}>
+                        {from}
+                        <span>–</span>
+                        {to}
+                      </span>
+                      <span className={styles.section}>{section}</span>
+                      <a
+                        className={styles.locationLink}
+                        href={location.gmapUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {location.name}
+                        <FiExternalLink className={styles.externalIcon} />
+                      </a>
+                    </div>
+                  )
+                )}
+              </Fragment>
+            ))}
+            <span className={styles.midday} />
+          </div>
 
-          {/* <div className={styles.announcement}>
-            <FiInfo className={styles.announcementIcon} />
+          <div className={styles.announcement}>
+            <FiSun className={styles.announcementIcon} />
             <p>
-              En raison de la situation sanitaire, les entraînements
-              ados/adultes se déroulent actuellement en extérieur, parc Karl
-              Marx, et en visioconférence. Pour en savoir plus ou faire une
-              scéance d’essai,{' '}
-              <a
-                href="https://fontainetkd.typeform.com/to/YlcrgiPQ"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                contactez-nous
-              </a>
-              .
+              En juillet/août, nous organisons des scéances de Fight Fitness
+              pour adultes les mardis et samedis matins aux parcs Pompidou à
+              Grenoble et Karl Marx à Fontaine. Ces scéances sont gratuites et
+              ouvertes à tous, sans adhésion, donc venez en profiter ! Le
+              calendrier exact dépendra des disponibilités de Cassie, notre
+              instructrice. Mieux vaut donc l’informer de votre venue à l’avance{' '}
+              <a href="https://www.facebook.com/FontaineTKD/">via Facebook</a>.
             </p>
-          </div> */}
+          </div>
         </div>
       </div>
     </section>
