8f23612957da59ccd283886c57b0f8162a8feed0
Axel Bocciarelli <axelboc@gmail.com>
2022-08-31 16:07:10 +0200

    Add text about registering

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,18 @@ import { root, icon } from './Footer.module.css';
 function Footer() {
   return (
     <footer className={root}>
-      <FiShare2 className={icon} />
-      Retrouvez-nous sur{' '}
-      <a href="https://www.facebook.com/FontaineTKD/">Facebook</a> et{' '}
-      <a href="https://www.instagram.com/fontainetkd/">Instagram</a>
+      <p>
+        Pour vous inscrire, venez nous rencontrer lors de notre prochain
+        entraînement, à partir du{' '}
+        <strong>lundi&nbsp;5&nbsp;septembre&nbsp;2022</strong>
+      </p>
+      <p>
+        <FiShare2 className={icon} />
+        Pour rester informé·e, retrouvez-nous sur{' '}
+        <a href="https://www.facebook.com/FontaineTKD/">Facebook</a>
+        &nbsp;et&nbsp;
+        <a href="https://www.instagram.com/fontainetkd/">Instagram</a>
+      </p>
     </footer>
   );
 }
