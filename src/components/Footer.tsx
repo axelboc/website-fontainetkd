aaff35bdad1c69a33ad3203ea3855f6d88c75349
Axel Bocciarelli <axelboc@gmail.com>
2025-06-09 16:25:50 +0200

    Add federation logos to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,47 @@
 import { FiShare2 } from 'react-icons/fi';
 
-import { root, icon } from './Footer.module.css';
+import {
+  root,
+  socialMedia,
+  icon,
+  federations,
+  iftLink,
+  itfLogo,
+  localFeds,
+} from './Footer.module.css';
+import itfLogoSrc from '../assets/itf-logo.png';
 
 function Footer() {
   return (
     <footer className={root}>
-      <p>
+      <p className={socialMedia}>
         <FiShare2 className={icon} />
         Pour rester informé·e, retrouvez-nous sur{' '}
         <a href="https://www.facebook.com/FontaineTKD/">Facebook</a>
         &nbsp;et&nbsp;
         <a href="https://www.instagram.com/fontainetkd/">Instagram</a>
       </p>
+      <div className={federations}>
+        <a
+          className={iftLink}
+          href="https://itftkd.sport/"
+          aria-label="ITF - International Taekwon-Do Federation"
+        >
+          <img
+            className={itfLogo}
+            src={itfLogoSrc}
+            width={320}
+            height={70}
+            alt=""
+          />
+        </a>
+        <p className={localFeds}>
+          <a href="https://www.taekwondo-itf-france.fr/">ITF France</a> ·{' '}
+          <a href="https://itfeurope.org/">
+            <abbr title="All Europe Taekwon-Do Federation">ITF Europe</abbr>
+          </a>
+        </p>
+      </div>
     </footer>
   );
 }
